3824826949ff500aefa43e07a9770ce9203cb732
Vira <vira@uab.edu>
2024-01-18 07:48:57 -0600

    Version 1

---
+++ src/assets/data.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+        question: "What is the national flower of India?",
+        option1: "Marigold",
+        option2: "Lotus",
+        option3: "Rose",
+        option4: "Lily",
+        ans: 2,
+    },
+    {
+        question: "What is the longest river in India?",
+        option1: "Ganges",
+        option2: "Indus",
+        option3: "Kaveri",
+        option4: "Yamuna",
+        ans: 1,
+    },
+    {
+        question: "What is the currency of India?",
+        option1: "Dollar",
+        option2: "Yen",
+        option3: "Euro",
+        option4: "Rupee",
+        ans: 4,
+    },
+    {
+        question: "Where is the Taj Mahal Located?",
+        option1: "Jaipur",
+        option2: "Agra",
+        option3: "Kerala",
+        option4: "Pune",
+        ans: 2,
+    },
+    {
+        question: "What is the national Animal of India?",
+        option1: "Asian Elephant",
+        option2: "Cow",
+        option3: "Bengal Tiger",
+        option4: "Monkey",
+        ans: 3,
+    },
+
+
+
+
+
+];
